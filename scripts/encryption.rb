51e4c16e526faae01c8148f8c34494e96d907650
Gdy985 <diyuangao2023@u.northwestern.edu>
2023-02-21 01:46:38 +0000

    second commit

--- scripts/encryption.rb
+++ scripts/encryption.rb
@@ -4,5 +4,13 @@
 
 # TODO:
 # 1. encrypt a secret and puts encrypted string
+secret = "tacos"
+puts secret
 # 2. prepare encrypted string for testing
+encrypted_string = BCrypt::Password.create(secret)
+puts encrypted_string
+
+user_entered_secret = "hahaha"
+result = BCrypt::Password.new(encrypted_string) == user_entered_secret
 # 3. test secret against prepared encrypted string
+puts result
